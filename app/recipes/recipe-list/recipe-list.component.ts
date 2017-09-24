3cceac1e231db4891141616af74e4196b3b67a95
Diego Kremer <diego.kremer@gmail.com>
2017-09-23 23:58:06 -0300

    Recipes model and recipe-list layout
    Created recipes model file and added further layout changes in HTML recipe-list component

--- app/recipes/recipe-list/recipe-list.component.ts
+++ app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipes.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('First Test Recipe', 'Just a test recipe', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg')
+  ];
 
   constructor() { }
 
