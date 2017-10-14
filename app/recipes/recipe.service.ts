39fb21ca12eb473e1ba7ae13b1ce1da9b1f323db
Diego Kremer <diego.kremer@gmail.com>
2017-10-14 19:07:57 -0300

    Added Ingredients to Recipe
    Added Ingredients to Recipe model and views

--- app/recipes/recipe.service.ts
+++ app/recipes/recipe.service.ts
@@ -1,16 +1,26 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('First Test Recipe', 
-                   'Just a test recipe', 
-                   'https://www.justonecookbook.com/wp-content/uploads/2016/09/Japanese-Pudding-600x400.jpg'),
-        new Recipe('Second Test Recipe', 
-                   'Just a test recipe and it is basically the same as before, so Im going to add a very big description now', 
-                   'http://www.thepuddingshop.co.uk/44/icky-sticky-toffee-steamed-pudding.jpg')
+        new Recipe('Japanese Pudding', 
+                   'A japanese pudding!', 
+                   'https://www.justonecookbook.com/wp-content/uploads/2016/09/Japanese-Pudding-600x400.jpg',
+                    [
+                        new Ingredient('Milk', 2),
+                        new Ingredient('Eggs', 6)
+                    ]),
+        new Recipe('Toffee Steamed Pudding', 
+                   'Pudding and Coffee? What else needs to be said?', 
+                   'http://www.thepuddingshop.co.uk/44/icky-sticky-toffee-steamed-pudding.jpg',
+                    [
+                        new Ingredient('Coffee', 1),
+                        new Ingredient('Milk', 2),
+                        new Ingredient('Sugar', 2)
+                    ])
       ];
 
       getRecipes() {
