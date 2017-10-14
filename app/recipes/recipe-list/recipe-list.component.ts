a8025f733ab591cabcc9cff3f9fe6f5f86be2e5b
Diego Kremer <diego.kremer@gmail.com>
2017-10-13 22:37:34 -0300

    RecipeService managing data
    Made RecipeService manage array data of Recipes

--- app/recipes/recipe-list/recipe-list.component.ts
+++ app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,18 +12,12 @@ export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('First Test Recipe', 
-               'Just a test recipe', 
-               'https://www.justonecookbook.com/wp-content/uploads/2016/09/Japanese-Pudding-600x400.jpg'),
-    new Recipe('Second Test Recipe', 
-               'Just a test recipe and it is basically the same as before, so Im going to add a very big description now', 
-               'http://www.thepuddingshop.co.uk/44/icky-sticky-toffee-steamed-pudding.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- app/recipes/recipe.service.ts
+++ app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('First Test Recipe', 
+                   'Just a test recipe', 
+                   'https://www.justonecookbook.com/wp-content/uploads/2016/09/Japanese-Pudding-600x400.jpg'),
+        new Recipe('Second Test Recipe', 
+                   'Just a test recipe and it is basically the same as before, so Im going to add a very big description now', 
+                   'http://www.thepuddingshop.co.uk/44/icky-sticky-toffee-steamed-pudding.jpg')
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
 }
